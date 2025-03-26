8426f67fbb554e1e43756c7528ff755a5f85cccf
piyushmehta09 <piyushmehtaa9@gmail.com>
2025-03-26 12:16:19 +0530

    Updated navigation bar

--- includes/navigation-bar.php
+++ includes/navigation-bar.php
@@ -7,9 +7,8 @@
             <li><a href="dashboard.html"><i class="fas fa-home"></i> Dashboard</a></li>
             <li><a href="home.php"><i class="fas fa-info-circle"></i> Home</a></li>
             <li><a href="about.php"><i class="fas fa-info-circle"></i> About</a></li>
-            <li><a href="product.php"><i class="fas fa-info-circle"></i> Products</a></li>
+            <li><a href="services.php"><i class="fas fa-info-circle"></i> services</a></li>
             <li><a href="mission.php"><i class="fas fa-info-circle"></i> mission</a></li>
-            <li><a href="vision.php"><i class="fas fa-info-circle"></i> vision</a></li>
             <li><a href="contact.php"><i class="fas fa-info-circle"></i> contact</a></li>
             <li><a href="forms.html"><i class="fas fa-file-alt"></i> Forms</a></li>
             
